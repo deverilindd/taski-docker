6407ac6ad702408b88e942adabb1234d2ba4393a
Deverilin Daniil <deverilin-dd@ntcees>
2025-10-02 22:20:15 +0300

    new ssh

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -4,8 +4,7 @@ from django.urls import include, path
 from rest_framework import routers
 
 router = routers.DefaultRouter()
-router.register('tasks', views.TaskView,
-                'task')
+router.register('tasks', views.TaskView, 'task')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
